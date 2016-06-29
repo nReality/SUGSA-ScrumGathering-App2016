96ef6d112295e2c6ff12576433c8ae6b77808029
Jacques de Vos <jacdevos@gmail.com>
2016-06-29 22:30:52 +0200

    First Agile Africa customisations

---
+++ app/pages/about/about.js
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  templateUrl: 'build/pages/about/about.html'
+})
+export class AboutPage {
+  constructor() {
+    this.conferenceDate = '2016-08-22';
+  }
+}
