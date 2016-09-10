81338404e92b2b9a8b4a70b08f5dc9192afb6f68
Jacques de Vos <jacdevos@gmail.com>
2016-09-10 15:37:29 +0200

    Make schedule the default tab

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -15,11 +15,11 @@ export class TabsPage {
   // set the root pages for each tab
   tab1Root: any = SchedulePage;
   tab2Root: any = SpeakerListPage;
-  tab3Root: any = MapPage;
+  tab3Root: any = AboutPage;
   tab4Root: any = AboutPage;
   mySelectedIndex: number;
 
   constructor(navParams: NavParams) {
-    this.mySelectedIndex = navParams.data.tabIndex || 3;
+    this.mySelectedIndex = navParams.data.tabIndex;
   }
 }
