3d619f818ae7cfe43af59fdac7e47aa6d31cdc80
Johan Barnard <johan@nreality.com>
2016-09-20 19:37:58 +0200

    Change buildscript to new keystore

--- build-scripts/android-sign.sh
+++ build-scripts/android-sign.sh
@@ -6,7 +6,7 @@ APK=platforms/android/build/outputs/apk/android-release-unsigned.apk
 OUT=android-release.apk
 
 a=${ANDROID_RELEASE_SIGN_PASS?"env variable ANDROID_RELEASE_SIGN_PASS not defined"}
-ANDROID_KEYSTORE=${ANDROID_KEYSTORE:-agile-africa-2016.keystore}
+ANDROID_KEYSTORE=${ANDROID_KEYSTORE:-SUGSA-conference-app.keystore}
 echo "using keystore file: $ANDROID_KEYSTORE"
 
 echo "signing"
