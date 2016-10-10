1175ee36d58106437c8c8bddbd19f7aaf4584fa2
johanb <johan@nreality.com>
2016-10-10 09:42:44 +0200

    Add constructor to sponsor page

--- app/pages/sponsors/sponsors.ts
+++ app/pages/sponsors/sponsors.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/sponsors/sponsors.html'
 })
 export class SponsorsPage {
-
+  constructor(public navCtrl: NavController){};  
 }
